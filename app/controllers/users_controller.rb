7c3b802c378e54f27e2adcedc3bd84508904470b
weilingooi <weilingooi62@gmail.com>
2020-08-16 04:32:31 +0800

    finish sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: :show
+    
+  def show; end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new user_params
+      
+    if @user.save
+      flash[:success] = t ".welcome"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".error"
+      render :new
+    end
+  end
+  
+  private
+  
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user.present?
+      
+    flash[:danger] = t ".user.user_not_found "
+    redirect_to root_path
+   end
+  
+  def user_params
+    params.require(:user).permit User::USERS_PARAMS
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   
   get "static_pages/home"
   get "static_pages/help"
+  resources :users, only: %i(new create show)
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 end
